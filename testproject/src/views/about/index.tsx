1a3e10363660f778df936bb50958183bee491d27
605358264 <605358264@qq.com>
2022-04-30 18:46:49 +0800

    请求和代理

--- testproject/src/views/about/index.tsx
+++ testproject/src/views/about/index.tsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import './index.less'
+import { getCarList } from "../../api/index";
 export default class About extends Component {
+  getList() {
+    getCarList({ page: "1" }).then((res) => console.log(res));
+  }
   render() {
     return (
       <div className="about">
         <div className="container">
           <h3 className="center"> About页面</h3>
           <p>欢迎来到关于我们页面！</p>
+          {/* 点击事件调用 */}
+          <button onClick={this.getList}>获取数据</button>
         </div>
       </div>
     );
